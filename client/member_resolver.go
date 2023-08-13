c0e765db8fb421fbb1392a9a5aff718b08634059
VenomPCPL <venompcpl@gmail.com>
2023-08-13 20:25:19 +0200

    feat(client/resolvers): Permissions / PermissionsIn

--- client/member_resolver.go
+++ client/member_resolver.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/BOOMfinity/bfcord/api"
 	"github.com/BOOMfinity/bfcord/discord"
+	"github.com/BOOMfinity/bfcord/discord/permissions"
 	"github.com/BOOMfinity/bfcord/errs"
+	"github.com/andersfylling/snowflake/v5"
 )
 
 var _ = (discord.GuildMemberQuery)(&memberResolver{})
@@ -28,6 +30,34 @@ func (mr memberResolver) VoiceState() (state *discord.VoiceState, err error) {
 		return &_state, nil
 	}
 	return nil, errs.ItemNotFound
+}
+
+func (mr memberResolver) Permissions() (perm permissions.Permission, err error) {
+	guild, err := mr.bot.Guild(mr.GuildID()).Get()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get guild: %w", err)
+	}
+	member, err := mr.bot.Guild(mr.GuildID()).Member(mr.ID()).Get()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get guild member: %w", err)
+	}
+	return discord.BasePermissions(member.Member, *guild), nil
+}
+
+func (mr memberResolver) PermissionsIn(channel snowflake.ID) (perm permissions.Permission, err error) {
+	guild, err := mr.bot.Guild(mr.GuildID()).Get()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get guild: %w", err)
+	}
+	member, err := mr.bot.Guild(mr.GuildID()).Member(mr.ID()).Get()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get guild member: %w", err)
+	}
+	ch, err := mr.bot.Channel(channel).Get()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get guild member: %w", err)
+	}
+	return discord.ChannelPermissions(*guild, member.Member, ch.Overwrites), nil
 }
 
 func (mr memberResolver) Get() (member *discord.MemberWithUser, err error) {
